f03b35c99de0c77fa534bbacad485aaac82f9b2f
Ben Garrett <code.by.ben@gmail.com>
2019-11-21 18:36:53 +1300

    cleanup

--- cmd/create.go
+++ cmd/create.go
@@ -19,6 +19,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/bengarrett/retrotxtgo/filesystem"
 	"github.com/spf13/cobra"
 )
 
@@ -34,20 +35,10 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
-		
+		filesystem.Read("textfiles/hi.txt") // fetched from filesystem/filesystem.go
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
