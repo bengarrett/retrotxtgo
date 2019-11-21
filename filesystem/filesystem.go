11ff7e7b1bd1b0ca5603a7ec01746c2fb716dc70
Ben Garrett <code.by.ben@gmail.com>
2019-11-21 21:59:12 +1100

    fix

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -14,13 +14,13 @@ func Read(path string) string {
 		log.Fatal(err)
 	}
 	stat, _ := os.Stat(path)
-	fmt.Printf("opening: %s\twhich is %v", stat.Name(), stat.Size())
+	fmt.Printf("opening: %s\twhich is %v bytes\n", stat.Name(), stat.Size())
 	data := make([]byte, stat.Size())
 	count, err := file.Read(data)
 	file.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	//fmt.Printf("read %d bytes: %q\n", count, data[:count])
 	return fmt.Sprintf("%s", data[:count])
 }
