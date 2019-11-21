899d6b4a2ce687769623293f2dc4fed7d9d2ff8a
Ben Garrett <code.by.ben@gmail.com>
2019-11-21 18:37:15 +1300

    +read and display file

---
+++ filesystem/filesystem.go
@@ -0,0 +1,22 @@
+//Package filesystem to handle the opening and reading of text files
+package filesystem
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//Read a text file and display its content
+func Read(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+}
