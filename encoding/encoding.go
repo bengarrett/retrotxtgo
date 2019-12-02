a3c31584afca0f5e52d63d20bf6a5a64f2d49d14
Ben Garrett <code.by.ben@gmail.com>
2019-12-02 11:06:29 +1100

    added byte order mark handlers and tests

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -4,12 +4,27 @@ package encoding
 //https://stackoverflow.com/questions/36111777/golang-how-to-read-a-text-file
 
 import (
+	"bytes"
 	"golang.org/x/net/html/charset"
 )
 
-//IsUTF8 blah
-func IsUTF8(b []byte) bool {
-	//todo limit read size?
+// BOM is the UTF-8 byte order mark prefix.
+var BOM = func() []byte {
+	return []byte{239, 187, 191} // 0xEF,0xBB,0xBF
+}
+
+// ToBOM adds a UTF-8 byte order mark if it doesn't already exist.
+func ToBOM(b []byte) []byte {
+	if len(b) > 2 {
+		if t := b[:3]; bytes.Equal(t, BOM()) == true {
+			return b
+		}
+	}
+	return append(BOM(), b...)
+}
+
+// UTF8 determines if a document is encoded as UTF-8.
+func UTF8(b []byte) bool {
 	_, name, _ := charset.DetermineEncoding(b, "text/plain")
 	if name == "utf-8" {
 		return true
--- encoding/encoding_test.go
+++ encoding/encoding_test.go
@@ -1,6 +1,53 @@
 package encoding
 
-func TestIsUTF8Empty() {
-	// xxx
+import (
+	"reflect"
+	"testing"
+)
 
+func TestUTF8(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	bom := append(BOM(), []byte("hello world")...)
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"empty string", args{}, false},
+		{"ascii string", args{[]byte("hello world")}, false},
+		{"arabic text", args{[]byte("مرحبا بالعالم")}, true},
+		{"cyrillic text", args{[]byte("Привіт, народ")}, true},
+		{"bom string", args{bom}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UTF8(tt.args.b); got != tt.want {
+				t.Errorf("IsUTF8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestToBOM(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{"empty string", args{}, []byte{239, 187, 191}},
+		{"ascii string", args{[]byte("hi")}, []byte{239, 187, 191, 104, 105}},
+		{"existing bom string", args{BOM()}, []byte{239, 187, 191}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToBOM(tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToBOM() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
