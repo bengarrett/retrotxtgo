96a214f2e7f7fca3282412882c8ba52ad3a45b08
Ben Garrett <code.by.ben@gmail.com>
2019-12-02 12:15:02 +1100

    +test

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -99,3 +99,20 @@ func Test_versionJSON(t *testing.T) {
 		})
 	}
 }
+
+func Test_versionText(t *testing.T) {
+	type args struct {
+		c bool
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			versionText(tt.args.c)
+		})
+	}
+}
