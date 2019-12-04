f59fc041962ab5a1fed734bb630634ea5bb77601
Ben Garrett <code.by.ben@gmail.com>
2019-12-04 13:28:59 +1100

    Execute() error switching

--- cmd/root.go
+++ cmd/root.go
@@ -158,7 +158,14 @@ func FileMissingErr() {
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	Check(ErrorFmt{"execute", "cobra", rootCmd.Execute()})
+	switch err := rootCmd.Execute(); fmt.Sprintf("%v", err) {
+	case "subcommand is required":
+		// ignored errors
+	default:
+		if err != nil {
+			Check(ErrorFmt{"execute", "cobra", err})
+		}
+	}
 }
 
 func init() {
