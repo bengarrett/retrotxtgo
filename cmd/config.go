7317d748df629e4e1a5071ecacff1a5ac767964a
Ben Garrett <code.by.ben@gmail.com>
2019-12-05 13:07:28 +1100

    set command is functional

--- cmd/config.go
+++ cmd/config.go
@@ -17,11 +17,15 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"runtime"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/alecthomas/chroma/quick"
@@ -30,39 +34,38 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+const shells string = "bash, powershell, zsh"
+
 var (
+	configShell   string
+	configSetName string
 	fileOverwrite bool
 	infoStyles    string
+	shellPreview  bool
 )
 
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: cp("Configure RetroTxt defaults"),
+	Short: "Configure RetroTxt defaults",
 	// no Run: means the help will be display
 }
 
 var configCreateCmd = &cobra.Command{
 	Use:   "create",
-	Short: cp("create a new config file"),
+	Short: "Create a new config file",
 	Run: func(cmd *cobra.Command, args []string) {
 		suppressCfg = true // todo: not working
 		if cfg := viper.ConfigFileUsed(); cfg != "" && fileOverwrite != true {
 			configExists(cmd.CommandPath(), "create")
 		}
-		bs, err := yaml.Marshal(viper.AllSettings())
-		Check(ErrorFmt{"could not create", "settings", err})
-		d, err := os.UserHomeDir()
-		Check(ErrorFmt{"could not use", "user home directory", err})
-		err = ioutil.WriteFile(d+"/.retrotxtgo.yaml", bs, 0660)
-		Check(ErrorFmt{"could not write", "settings", err})
-		fmt.Println("Created a new config file at:", cf(d+"/.retrotxtgo.yaml"))
+		writeConfig(false)
 	},
 }
 
 var configDeleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: cp("remove the default config file"),
+	Short: "Remove the default config file",
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := viper.ConfigFileUsed()
 		if cfg == "" {
@@ -81,7 +84,7 @@ var configDeleteCmd = &cobra.Command{
 
 var configEditCmd = &cobra.Command{
 	Use:   "edit",
-	Short: cp("edit the default config file"),
+	Short: "Edit the default config file",
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg := viper.ConfigFileUsed()
 		if cfg == "" {
@@ -120,7 +123,7 @@ var configEditCmd = &cobra.Command{
 
 var configInfoCmd = &cobra.Command{
 	Use:   "info",
-	Short: cp("view settings configured by the config"),
+	Short: "View settings configured by the config",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(cp("These are the default configurations used by the commands of RetroTxt when no flags are given.\n"))
 		sets, err := yaml.Marshal(viper.AllSettings())
@@ -130,23 +133,221 @@ var configInfoCmd = &cobra.Command{
 	},
 }
 
-// todo detect Windows to
-// + replace terminal shell with command prompt
-// + automatically use powershell
 var configShellCmd = &cobra.Command{
 	Use:   "shell",
-	Short: cp("include retrotxt autocompletion to the terminal shell"),
+	Short: "Include retrotxt autocompletion to the terminal shell",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("shell config file")
+		var buf bytes.Buffer
+		var lexer string
+		switch configShell {
+		case "bash":
+			lexer = "bash"
+			cmd.GenBashCompletion(&buf)
+		case "powershell", "posh":
+			lexer = "powershell"
+			cmd.GenPowerShellCompletion(&buf)
+		case "zsh":
+			lexer = "bash"
+			cmd.GenZshCompletion(&buf)
+		default:
+		}
+		quick.Highlight(os.Stdout, buf.String(), lexer, "terminal256", "monokai")
 	},
 }
 
 var configSetCmd = &cobra.Command{
 	Use:   "set",
-	Short: cp("change a configuration"),
+	Short: "Change a configuration",
+	//todo add long with information on how to view settings
+	Example: `--name create.meta.description # to change the meta description setting
+--name version.format          # to set the version command output format`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("set config file")
+		var name = configSetName
+		keys := viper.AllKeys()
+		sort.Strings(keys)
+		// sort.SearchStrings() - The slice must be sorted in ascending order.
+		var i = sort.SearchStrings(keys, name)
+		if i == len(keys) || keys[i] != name {
+			err := fmt.Errorf("retrotxt config info")
+			CheckFlag(ErrorFmt{"name", name, err})
+		}
+		s := viper.GetString(name)
+		switch s {
+		case "":
+			fmt.Printf("\n%s is currently disabled\n", cp(name))
+		default:
+			fmt.Printf("\n%s is currently set to %q\n", cp(name), s)
+		}
+		switch {
+		case name == "create.layout":
+			fmt.Printf("Set a new value, choices: %s\n", ci(createLayouts()))
+			promptSlice(createLayouts())
+		case name == "info.format":
+			fmt.Printf("Set a new value, choice: %s\n", ci(infoFormats))
+			promptSlice(infoFormats)
+		case name == "version.format":
+			fmt.Printf("Set a new value, choices: %s\n", ci(versionFormats))
+			promptSlice(versionFormats)
+		case name == "create.server-port":
+			fmt.Printf("Set a new HTTP port, choices: %v-%v (recommended: 8080)\n", portMin, portMax)
+			promptPort()
+		case name == "create.meta.generator":
+			fmt.Printf("<meta name=\"generator\" content=\"RetroTxt v%s\">\nEnable this element? [y/n]\n", Ver)
+			promptBool()
+		case s == "":
+			promptMeta(s)
+			fmt.Printf("\nSet a new value or leave blank to keep it disabled: \n")
+			promptString(s)
+		default:
+			promptMeta(s)
+			fmt.Printf("\nSet a new value, leave blank to keep as-is or use a dash [-] to disable: \n")
+			promptString(s)
+		}
 	},
+}
+
+func promptMeta(val string) {
+	s := strings.Split(configSetName, ".")
+	switch {
+	case len(s) != 3, s[0] != "create", s[1] != "meta":
+		return
+	}
+	elm := fmt.Sprintf("<meta name=\"%s\" value=\"%s\">", s[2], val)
+	var buf bytes.Buffer
+	err := quick.Highlight(&buf, elm, "html", "terminal256", "lovelace")
+	if err != nil {
+		fmt.Printf("\n%s\n", elm)
+	} else {
+		fmt.Printf("\n%v\n", buf.String())
+	}
+	fmt.Println(cf("About this element: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta#attr-name"))
+}
+
+func promptString(keep string) {
+	// allow multiple word user input
+	scanner := bufio.NewScanner(os.Stdin)
+	var save string
+	for scanner.Scan() {
+		txt := scanner.Text()
+		switch txt {
+		case "":
+			os.Exit(0)
+		case "-":
+			save = ""
+		default:
+			save = txt
+		}
+		viper.Set(configSetName, save)
+		fmt.Printf("%s %s is now set to \"%v\"\n", cs("✓"), cp(configSetName), save)
+		writeConfig(true)
+		os.Exit(0)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
+	}
+
+}
+
+func promptBool() {
+	var input string
+	cnt := 0
+	for {
+		input = ""
+		cnt++
+		fmt.Scanln(&input)
+		if input == "" {
+			promptCheck(cnt)
+			continue
+		}
+		switch input {
+		case "n", "no", "f", "false":
+			viper.Set(configSetName, false)
+			fmt.Printf("%s %s is now disabled\n", cs("✓"), cp(configSetName))
+		case "y", "yes", "t", "true":
+			viper.Set(configSetName, true)
+			fmt.Printf("%s %s is now enabled\n", cs("✓"), cp(configSetName))
+		default:
+			fmt.Printf("%s %v\n", ce("✗"), input)
+			promptCheck(cnt)
+			continue
+		}
+		writeConfig(true)
+		os.Exit(0)
+	}
+}
+
+func promptSlice(s string) {
+	keys := strings.Split(s, ", ")
+	sort.Strings(keys)
+	var input string
+	cnt := 0
+	for {
+		input = ""
+		cnt++
+		fmt.Scanln(&input)
+		if input == "" {
+			promptCheck(cnt)
+			continue
+		}
+		var i = sort.SearchStrings(keys, input)
+		if i >= len(keys) || keys[i] != input {
+			fmt.Printf("%s %v\n", ce("✗"), input)
+			promptCheck(cnt)
+			continue
+		}
+		viper.Set(configSetName, input)
+		fmt.Printf("%s %s is now set to \"%v\"\n", cs("✓"), cp(configSetName), input)
+		writeConfig(true)
+		os.Exit(0)
+	}
+}
+
+// ValidPort checks that p can be used as a network port value
+func ValidPort(p int) bool {
+	if p < portMin || p > portMax {
+		return false
+	}
+	return true
+}
+
+func promptCheck(cnt int) {
+	switch {
+	case cnt == 2:
+		fmt.Println("Ctrl+C to keep the existing port")
+	case cnt >= 4:
+		os.Exit(1)
+	}
+}
+
+func promptPort() {
+	var input string
+	cnt := 0
+	for {
+		input = ""
+		cnt++
+		fmt.Scanln(&input)
+		if input == "" {
+			promptCheck(cnt)
+			continue
+		}
+		i, err := strconv.ParseInt(input, 10, 0)
+		if err != nil && input != "" {
+			fmt.Printf("%s %v\n", ce("✗"), input)
+			promptCheck(cnt)
+			continue
+		}
+		// check that the input a valid port
+		if v := ValidPort(int(i)); v == false {
+			fmt.Printf("%s %v, is out of range\n", ce("✗"), input)
+			promptCheck(cnt)
+			continue
+		}
+		viper.Set(configSetName, i)
+		fmt.Printf("%s %s is now set to \"%v\"\n", cs("✓"), cp(configSetName), i)
+		writeConfig(true)
+		os.Exit(0)
+	}
 }
 
 func init() {
@@ -156,26 +357,16 @@ func init() {
 	configCmd.AddCommand(configDeleteCmd)
 	configCmd.AddCommand(configEditCmd)
 	configCmd.AddCommand(configInfoCmd)
-	configInfoCmd.Flags().StringVarP(&infoStyles, "syntax-style", "c", "monokai", "Config syntax highligher, use "+ci("none")+" to disable")
+	configInfoCmd.Flags().StringVarP(&infoStyles, "syntax-style", "c", "monokai", "config syntax highligher, use "+ci("none")+" to disable")
 	configCmd.AddCommand(configShellCmd)
+	configShellCmd.Flags().StringVarP(&configShell, "interpreter", "i", "", "user shell to receive retrotxtgo auto-completions\nchoices: "+ci(shells))
+	//configShellCmd.Flags().BoolVar(&shellPreview, "preview", false, "prints the shell completion instead of applying it")
+	configShellCmd.MarkFlagRequired("interpreter")
+	configShellCmd.SilenceErrors = true
 	configCmd.AddCommand(configSetCmd)
-
-	// todo
-	// include text on how to use
-	// rootCmd.GenBashCompletionFile("hi.sh")
-	// rootCmd.GenBashCompletionFile("hi.sh")
-	// rootCmd.GenPowershellCompletionFile("hi.sh")
-	// rootCmd.GenZshCompletionFile("hi.sh")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	configSetCmd.Flags().StringVarP(&configSetName, "name", "n", "", `the configuration path to edit in dot syntax (see examples)
+to see a list of names run: retrotxt config info`)
+	configSetCmd.MarkFlagRequired("name")
 }
 
 func prompt(query string, yesDefault bool) bool {
@@ -211,6 +402,20 @@ func configExists(name string, suffix string) {
 	fmt.Printf("To edit it: %s\n", cp(cmd+"edit"))
 	fmt.Printf("To delete:  %s\n", cp(cmd+"delete"))
 	os.Exit(1)
+}
+
+func writeConfig(update bool) {
+	bs, err := yaml.Marshal(viper.AllSettings())
+	Check(ErrorFmt{"could not create", "settings", err})
+	d, err := os.UserHomeDir()
+	Check(ErrorFmt{"could not use", "user home directory", err})
+	err = ioutil.WriteFile(d+"/.retrotxtgo.yaml", bs, 0660)
+	Check(ErrorFmt{"could not write", "settings", err})
+	s := "Created a new"
+	if update == true {
+		s = "Updated the"
+	}
+	fmt.Println(s+" config file at:", cf(d+"/.retrotxtgo.yaml"))
 }
 
 /*
