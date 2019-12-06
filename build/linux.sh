85fef8d81edb9b58a4c0051fd15929487e1f748c
Ben Garrett <code.by.ben@gmail.com>
2019-12-06 18:29:34 +1100

    build & artifacts

--- build/linux.sh
+++ build/linux.sh
@@ -4,7 +4,7 @@
 export RTG_COMMIT=$(git rev-list --abbrev-commit -1 HEAD) && \
 export RTG_HEADCNT=$(git rev-list --count HEAD) && \
 export RTG_DATE=$(date --utc +%H:%M:%S/%Y-%m-%d) && \
-go build -ldflags "-X github.com/bengarrett/retrotxtgo/cmd.GoBuildGitCommit=$RTG_COMMIT \
+go build -o artifacts/retrotxt -ldflags "-X github.com/bengarrett/retrotxtgo/cmd.GoBuildGitCommit=$RTG_COMMIT \
 -X github.com/bengarrett/retrotxtgo/cmd.GoBuildGitCount=$RTG_HEADCNT \
 -X github.com/bengarrett/retrotxtgo/cmd.GoBuildDate=$RTG_DATE" && \
-./retrotxtgo version
+./artifacts/retrotxt version
