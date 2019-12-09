2f8862d5ea104008761b1d2a0441c95023780b3d
Ben Garrett <code.by.ben@gmail.com>
2019-12-09 12:17:40 +1100

    + a working Table() func

--- cmd/view.go
+++ cmd/view.go
@@ -16,9 +16,14 @@ limitations under the License.
 package cmd
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/bengarrett/retrotxtgo/codepage"
+
+	"golang.org/x/text/encoding/japanese"
 
 	"github.com/bengarrett/retrotxtgo/filesystem"
 
@@ -26,6 +31,22 @@ import (
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/encoding/ianaindex"
+)
+
+const viewFormats string = "color, text"
+
+type iana struct {
+	mime  string
+	index string
+	mib   string
+	s     []string
+}
+
+var (
+	viewCodePage string = "ibm437"
+	viewFilename string
+	viewFormat   string
+	viewWidth    int
 )
 
 // viewCmd represents the view command
@@ -39,124 +60,44 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//		name := "textfiles/ZII-RTXT.ans"
-		//name := "/home/ben/hx_back.ans"
-		name := "textfiles/hi.txt"
-		fmt.Println("view called", name)
-
-		file, err := os.Open(name)
 
 		// todo reverse scan of file looking for SAUCE00 and COMNTT
-
-		//defer close(name)
-		Check(ErrorFmt{"file open", name, err})
-		defer file.Close()
-
-		decode := charmap.CodePage437.NewDecoder()
-
-		scanner := bufio.NewScanner(file)
-
 		// todo scan for unique color codes like 24-bit color
-
 		// todo scan for new lines or character counts and hard-code the width
 
-		for scanner.Scan() {
-			out, _ := decode.Bytes(scanner.Bytes())
-			fmt.Printf("%s%s\n", string(out), "\033[0m")
+		if viewFilename == "" {
+			// todo: remove from final
+			viewFilename = "textfiles/cp-437-all-characters.txt"
 		}
-		//fmt.Printf("%v", charmap.Charmap())
+		// todo handle unchanged viewCodePage, where UTF8 encoding will be checked otherwise use
+		encoding, err := ianaindex.IANA.Encoding(viewCodePage)
+		// todo handle invalid encoding value, with notice on how to display the list
+		Check(ErrorFmt{"encoding transform", viewCodePage, err})
+		var d codepage.Set
 
-		// buf := make([]byte, 32*1024) //
+		data, err := filesystem.Read(viewFilename)
+		Check(ErrorFmt{"file open", viewFilename, err})
 
-		// for {
-		// 	n, err := file.Read(buf)
-		// 	if n > 0 {
-		// 		fmt.Print(buf[:n])
-		// 	}
-		// 	if err == io.EOF {
-		// 		break
-		// 	}
-		// 	if err != nil {
-		// 		log.Printf("read %d bytes: %v", n, err)
-		// 	}
-		// }
+		d.Transform(data, encoding)
+		d.SwapAll(true)
+		fmt.Printf("\n%s\n", d.Data)
 
-		// r := bufio.NewReaderSize(f, 100)
-		// b, _ := r.
-		// //b, _ := r.Peek(100)
+		println(codepage.Table(""))
 
-		// decode := charmap.CodePage437.NewDecoder()
-		// out, _ := decode.Bytes(b)
-		// fmt.Println(out)
-
-		//func DetermineEncodingFromReader(r io.Reader) (e encoding.Encoding, name string, certain bool, err error) {
-		// b, err := bufio.NewReader(f).Peek(1024)
-		// if err != nil {
-		// 	return
-		// }
-
-		// e, name, certain := charset.DetermineEncoding(b, "")
-		// fmt.Println(e, name, certain)
-		// //}
-
-		//encoder := x.text.encoding.NewEncoder()
-		//scanner := bufio.NewScanner(f)
-		// var buf bytes.Buffer
-		// for scanner.Scan() {
-		// 	buf.Write(scanner.Bytes())
-		// }
-		// fmt.Println(buf.String())
-
+		// fmt.Printf("\n%s\n", data)
+		// // todo: make an --example that auto generates a table bytes 0 - 255 | lf every 16 characters
+		// oof := []byte{48, 49, 50, 51, 52}
+		// fmt.Printf("\n%v\t%d\n", oof, oof)
 	},
-}
-
-func transform(e encoding.Encoding) {
-	name := "textfiles/cp-437-all-characters.txt"
-	data, err := filesystem.Read(name)
-	Check(ErrorFmt{"file open", name, err})
-	iana, err := ianaindex.IANA.Name(e)
-	if err != nil {
-		iana = "unknown"
-	}
-	utf8decode, err := e.NewDecoder().Bytes(data)
-	Check(ErrorFmt{"encoding transform", iana, err})
-
-	fmt.Printf("\n%s\n", utf8decode)
 }
 
 var viewCodePagesCmd = &cobra.Command{
 	Use:   "codepages",
-	Short: "List supported and available legacy codepages that can be converted into UTF-8",
+	Short: "list available legacy codepages that RetroTxt can convert into UTF-8",
 	Run: func(cmd *cobra.Command, args []string) {
-		// https://godoc.org/golang.org/x/text/encoding/charmap
-		// fmt.Printf("%v\n", charmap.All)
-
-		// fmt.Printf("%v\n", charmap.CodePage037))
-		// // for _, name := range charmap.All {
-		// // 	fmt.Println(name, charmap{name})
-		// // }
-		e, _ := ianaindex.IANA.Encoding("cp437")
-
-		transform(e)
-
-		fmt.Println(ianaindex.IANA.Name(e))
-
-		fmt.Println(ianaindex.MIME.Name(charmap.ISO8859_7))
-		fmt.Println(ianaindex.IANA.Name(charmap.ISO8859_7))
-		fmt.Println(ianaindex.MIB.Name(charmap.ISO8859_7))
-
-		fmt.Println(ianaindex.MIME.Encoding("437"))
+		fmt.Println(codepages())
 	},
 }
-
-const viewFormats string = "color, text"
-
-var (
-	viewCodePage string
-	viewFilename string
-	viewFormat   string
-	viewWidth    int
-)
 
 func init() {
 	rootCmd.AddCommand(viewCmd)
@@ -170,4 +111,76 @@ func init() {
 	viewCmd.MarkFlagRequired("name")
 	viewCmd.Flags().SortFlags = false
 	viewCmd.AddCommand(viewCodePagesCmd)
+}
+
+// codepages returns a tabled list of supported IANA character set encodings
+func codepages() string {
+	// create a buffer and writer for tab formatting
+	var buf bytes.Buffer
+	w := new(tabwriter.Writer)
+	w.Init(&buf, 0, 8, 0, '\t', 0)
+
+	var ii iana
+	var err error
+	fmt.Fprintln(w, cp("\nSupported legacy codepages and encodings"))
+	fmt.Fprintln(w, cf(strings.Repeat("\u2015", 40)))
+	fmt.Fprintf(w, "\ttitle\talias(s)\n")
+	c := append(charmap.All, japanese.All...)
+	for _, n := range c {
+		name := fmt.Sprint(n)
+		if name == "X-User-Defined" {
+			continue
+		}
+		ii.mime, err = ianaindex.MIME.Name(n)
+		if err != nil {
+			continue
+		}
+		ii.index, _ = ianaindex.IANA.Name(n)
+		ii.mib, _ = ianaindex.MIB.Name(n)
+		ii.s = strings.Split(name, " ")
+		// display encoding name and alias
+		fmt.Fprintf(w, "\t%s\t%s", name, ci(ii.mib))
+		// create common use CP aliases
+		switch {
+		case ii.s[0] == "IBM":
+		case ii.s[0] == "Windows" && ii.s[1] == "Code":
+			fmt.Fprintf(w, "\tCP%s", ii.s[3])
+		case ii.s[0] == "Windows":
+			fmt.Fprintf(w, "\tCP%s", ii.s[1])
+		default:
+			fmt.Fprintf(w, "\t")
+		}
+		// only show MIME if it is different to the previous aliases
+		switch {
+		case strings.ToLower(strings.ReplaceAll(name, "-", " ")) == strings.ToLower(strings.ReplaceAll(ii.mime, "-", " ")):
+		case strings.ReplaceAll(name, "-", "") == strings.ReplaceAll(ii.mime, "-", ""):
+		case ii.mib == ii.mime:
+			fmt.Fprintf(w, "\t%s", cf(""))
+		default:
+			fmt.Fprintf(w, "\t%s", cf(ii.mime))
+		}
+		fmt.Fprint(w, "\n")
+	}
+	fmt.Fprint(w, "\ttitle\talias(s)\n")
+	fmt.Fprint(w, "\n"+cinf("*")+" Code Page 437 ("+cc("CP437")+") is commonly used by MS-DOS English text and ANSI art")
+	fmt.Fprint(w, "\n  ISO 8859-1 ("+cc("ISOLatin1")+") is found in legacy Internet, Unix and Amiga documents")
+	fmt.Fprint(w, "\n  Windows 1252 ("+cc("windows1252")+") is found in legacy English language Windows operating systems")
+	w.Flush()
+	return buf.String()
+}
+
+// transform opens a text file and converts it from the Encoding legacy character set into UTF-8.
+func transform(name string, e encoding.Encoding) codepage.Set {
+	var s codepage.Set
+	data, err := filesystem.Read(name)
+	Check(ErrorFmt{"file open", name, err})
+	iana, err := ianaindex.IANA.Name(e)
+	if err != nil {
+		iana = "unknown"
+	}
+	Check(ErrorFmt{"IANA index", iana, err})
+	decode, err := e.NewDecoder().Bytes(data)
+	Check(ErrorFmt{"encoding transform", iana, err})
+	s.Data = decode
+	return s
 }
