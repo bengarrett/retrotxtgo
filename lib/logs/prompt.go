961cb67e391c4e2259d2dd6ae2ca1c7472138013
Ben Garrett <code.by.ben@gmail.com>
2020-05-19 09:45:17 +1000

    Commented functions and refactored some code.

--- lib/logs/prompt.go
+++ lib/logs/prompt.go
@@ -4,123 +4,114 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-// PromptPort ...
-func PromptPort(validate bool) (port uint) {
-	var input string
-	cnt := 0
-	for {
-		input = ""
-		cnt++
-		fmt.Scanln(&input)
-		if input == "" {
-			promptCheck(cnt)
-			continue
+func promptCheck(prompts int) {
+	switch {
+	case prompts == 2:
+		if i, err := fmt.Println("Ctrl+C to keep the existing value"); err != nil {
+			log.Fatalf("logs.promptCheck println at %dB: %s", i, err)
 		}
-		i, err := strconv.ParseInt(input, 10, 0)
-		if err != nil {
-			fmt.Printf("%s %v\n", Ce("✗"), input)
-			promptCheck(cnt)
-			continue
-		}
-		if validate {
-			if v := PortValid(uint(i)); !v {
-				fmt.Printf("%s %v, is out of range\n", Ce("✗"), input)
-				promptCheck(cnt)
-				continue
-			}
-		}
-		return uint(i)
+	case prompts >= 4:
+		os.Exit(1)
 	}
 }
 
-// PortValid ...
-func PortValid(p uint) bool {
-	if p < PortMin || p > PortMax {
+// PromptPort asks for and returns a HTTP port value.
+func PromptPort(validate bool) (port uint) {
+	input, prompts := "", 0
+	for {
+		input = ""
+		prompts++
+		if i, err := fmt.Scanln(&input); err != nil {
+			log.Fatalf("logs.promptport scanln at %d: %s", i, err)
+		}
+		if input == "" {
+			promptCheck(prompts)
+			continue
+		}
+		value, err := strconv.ParseInt(input, 10, 0)
+		if err != nil {
+			fmt.Printf("%s %v\n", Ce("✗"), input)
+			promptCheck(prompts)
+			continue
+		}
+		port = uint(value)
+		if validate {
+			if v := PortValid(port); !v {
+				fmt.Printf("%s %v, is out of range\n", Ce("✗"), input)
+				promptCheck(prompts)
+				continue
+			}
+		}
+		return port
+	}
+}
+
+// PortValid checks if the network port is within range to serve HTTP.
+func PortValid(port uint) (ok bool) {
+	if port < PortMin || port > PortMax {
 		return false
 	}
 	return true
 }
 
-// PromptString ...
-func PromptString() (value string) {
-	// allow multiple words
+// PromptString asks for and returns a multi-word string.
+func PromptString() (words string) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		txt := scanner.Text()
-		switch txt {
+		words := scanner.Text()
+		switch words {
 		case "":
 			os.Exit(0)
 		case "-":
-			value = ""
-		default:
-			value = txt
+			words = ""
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		os.Exit(1)
+		log.Fatalf("reading standard input: %s", err)
 	}
-	return value
+	return words
 }
 
-// PromptStrings ...
-func PromptStrings(keys []string) (value string) {
-	cnt := 0
+// PromptStrings asks for and returns a single choice from a collection of keys.
+func PromptStrings(keys []string) (key string) {
+	prompts := 0
 	for {
-		value = ""
-		cnt++
-		fmt.Scanln(&value)
-		if value == "" {
-			promptCheck(cnt)
+		key = ""
+		prompts++
+		if i, err := fmt.Scanln(&key); err != nil {
+			log.Fatalf("logs.promptstrings scanln at %d: %s", i, err)
+		}
+		if key == "" {
+			promptCheck(prompts)
 			continue
 		}
-		var i = sort.SearchStrings(keys, value)
-		if i >= len(keys) || keys[i] != value {
-			fmt.Printf("%s %v\n", Ce("✗"), value)
-			promptCheck(cnt)
+		var i = sort.SearchStrings(keys, key)
+		if i >= len(keys) || keys[i] != key {
+			fmt.Printf("%s %v\n", Ce("✗"), key)
+			promptCheck(prompts)
 			continue
 		}
-		return value
+		return key
 	}
 }
 
-func promptCheck(cnt int) {
-	switch {
-	case cnt == 2:
-		fmt.Println("Ctrl+C to keep the existing value")
-	case cnt >= 4:
-		os.Exit(1)
-	}
-}
-
-// PromptYN asks the user for a yes or no input.
-func PromptYN(query string, yesDefault bool) bool {
-	var y, n string = "Y", "n"
+// PromptYN asks for a yes or no input.
+func PromptYN(ask string, yesDefault bool) bool {
+	y, n := "Y", "n"
 	if !yesDefault {
 		y, n = "y", "N"
 	}
-	fmt.Printf("%s? [%s/%s] ", query, y, n)
+	fmt.Printf("%s? [%s/%s] ", ask, y, n)
 	input, err := promptRead(os.Stdin)
 	Log(err)
-	return promptyn(input, yesDefault)
-}
-
-func promptyn(input string, yesDefault bool) bool {
-	switch input {
-	case "":
-		if yesDefault {
-			return true
-		}
-	case "yes", "y":
-		return true
-	}
-	return false
+	return parseyn(input, yesDefault)
 }
 
 func promptRead(stdin io.Reader) (input string, err error) {
@@ -131,4 +122,16 @@ func promptRead(stdin io.Reader) (input string, err error) {
 		return input, err
 	}
 	return input, nil
+}
+
+func parseyn(input string, yesDefault bool) bool {
+	switch input {
+	case "":
+		if yesDefault {
+			return true
+		}
+	case "yes", "y":
+		return true
+	}
+	return false
 }
