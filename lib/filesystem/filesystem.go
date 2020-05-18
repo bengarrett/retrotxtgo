c0cd1a6af815d5f9ef20ce16537c60f689ea7237
Ben Garrett <code.by.ben@gmail.com>
2020-05-18 20:10:14 +1000

    Improved comments and some code refactoring.

--- lib/filesystem/filesystem.go
+++ lib/filesystem/filesystem.go
@@ -4,31 +4,65 @@ package filesystem
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 )
 
-// Read opens and returns the content of the name file.
-func Read(name string) ([]byte, error) {
+// Read opens and returns the content of the named file.
+func Read(name string) (data []byte, err error) {
 	// check name is file not anything else
-	data, err := ReadAllBytes(name)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
+	return ReadAllBytes(name)
 }
 
-//TailBytes reads the name file from the offset position relative to the end of the file.
-func TailBytes(name string, offset int64) ([]byte, error) {
+// ReadAllBytes reads the named file and returns the content as a byte array.
+func ReadAllBytes(name string) (data []byte, err error) {
 	file, err := os.Open(name)
 	if err != nil {
 		return nil, err
 	}
+	// bufio is the most performant
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data = append(data, scanner.Bytes()...)
+	}
+	if err = scanner.Err(); err != nil {
+		return data, err
+	}
+	if err = file.Close(); err != nil {
+		return data, err
+	}
+	err = scanner.Err()
+	return data, err
+}
+
+// ReadChunk reads a section of the named file and returns the result.
+// TODO: finish
+func ReadChunk(name string, size int) (data []byte, err error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return data, err
+	}
+	defer file.Close()
+	read := bufio.NewReaderSize(file, size)
+	buf := make([]byte, size)
+	_, err = read.Read(buf)
+	if err != nil {
+		return data, err
+	}
+	//seek logic goes here
+	return buf, err
+}
+
+// ReadTail reads the named file from the offset position relative to the end of the file.
+func ReadTail(name string, offset int64) (data []byte, err error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return data, err
+	}
 	defer file.Close()
 	var size int64 = int64(math.Abs(float64(offset)))
 	if stat, err := os.Stat(name); err == nil && stat.Size() < size {
-		return nil, fmt.Errorf("offset: value is %v too large for a %v byte file", offset, stat.Size())
+		return data, fmt.Errorf("offset: value is %v too large for a %v byte file", offset, stat.Size())
 	}
 	// file.Seek(whence)
 	// 0 means relative to the origin of the file
@@ -36,43 +70,11 @@ func TailBytes(name string, offset int64) ([]byte, error) {
 	// 2 means relative to the end
 	if _, err = file.Seek(offset, 2); err != nil {
 		// todo: have offset deal with runes not bytes
-		return nil, err
+		return data, err
 	}
-	buffer := make([]byte, size)
-	if _, err = file.Read(buffer); err != nil {
-		return nil, err
+	data = make([]byte, size)
+	if _, err = file.Read(data); err != nil {
+		return data, err
 	}
-	return buffer, nil
-}
-
-//ReadAllBytes reads the named file and returns its content as a byte array.
-func ReadAllBytes(name string) ([]byte, error) {
-	file, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	buffer, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-	return buffer, nil
-}
-
-// ReadChunk is unused
-func ReadChunk(name string, size int) ([]byte, error) {
-	file, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	read := bufio.NewReaderSize(file, size)
-	buf := make([]byte, size)
-
-	_, err = read.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-	// do things
-	return buf, nil
+	return data, err
 }
