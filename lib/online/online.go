c15aa60b90cec63e02306b77b4e23f1b476e084a
Ben Garrett <code.by.ben@gmail.com>
2020-06-13 14:53:55 +1000

    Commented functions.

--- lib/online/online.go
+++ lib/online/online.go
@@ -8,38 +8,28 @@ import (
 	"time"
 )
 
-const userAgent = "retrotxt ping"
+const userAgent = "retrotxt version ping"
 const httpTimeout = time.Second * 3
 
-// API blah
+// API interface to store the JSON results from GitHub
 type API map[string]interface{}
 
 // ReleaseAPI GitHub API v3 releases endpoint
 // See: https://developer.github.com/v3/repos/releases/
 const ReleaseAPI = "https://api.github.com/repos/bengarrett/retrotxtgo/releases/latest"
 
-/*
-access-control-expose-headers: ETag
-cache-control: public, max-age=60, s-maxage=60
-last-modified: Fri, 12 Jun 2020 01:04:23 GMT
-etag: W/"3715383704fac6f3568e9039b347937a"
-*/
-/*
-cache-control: no-cache
-If-Modified-Since: Fri, 12 Jun 2020 01:04:23 GMT
-If-None-Match: W/"3715383704fac6f3568e9039b347937a"
-*/
-
-// Endpoint ..
+// Endpoint request an API endpoint from the URL.
+// A HTTP ETag can be provided to validate local data cache against the server.
+// The useCache will return true with the etag value matches the server's ETag header.
 func Endpoint(etag, url string) (useCache bool, data API, err error) {
 	useCache = false
-	header, body, err := Get(url, etag)
+	resp, body, err := Get(url, etag)
 	if err != nil {
 		return useCache, data, err
 	}
 	if etag != "" {
-		s := header.Get("Status")
-		if s == "304 Not Modified" || (s == "200 OK" && data == nil) {
+		s := resp.StatusCode
+		if s == 304 || (s == 200 && data == nil) {
 			// Not Modified
 			return true, data, nil
 		}
@@ -48,12 +38,13 @@ func Endpoint(etag, url string) (useCache bool, data API, err error) {
 		return useCache, data, errors.New("the response from is not in json syntax: " + url)
 	}
 	err = json.Unmarshal(body, &data)
-	data["etag"] = header.Get("Etag")
+	data["etag"] = resp.Header.Get("Etag")
 	return useCache, data, nil
 }
 
-// Get ...
-func Get(url, etag string) (header http.Header, body []byte, err error) {
+// Get fetches a URL and returns both its response and body.
+// If an etag is provided a "If-None-Match" header request will be included.
+func Get(url, etag string) (resp *http.Response, body []byte, err error) {
 	client := &http.Client{
 		Timeout: httpTimeout,
 	}
@@ -65,16 +56,16 @@ func Get(url, etag string) (header http.Header, body []byte, err error) {
 		req.Header.Set("If-None-Match", etag)
 	}
 	req.Header.Set("User-Agent", userAgent)
-	resp, err := client.Do(req)
+	resp, err = client.Do(req)
 	if err != nil {
 		return nil, body, err
 	}
 	defer resp.Body.Close()
 	body, err = ioutil.ReadAll(resp.Body)
-	return resp.Header, body, err
+	return resp, body, err
 }
 
-// Ping requests a URL and determines if its status is ok.
+// Ping requests a URL and determines if the status is ok.
 func Ping(url string) (ok bool, err error) {
 	ok = false
 	client := &http.Client{
