4cbc66fbfe68b739a530af94c7a07871c1258441
Ben Garrett <code.by.ben@gmail.com>
2020-06-17 19:48:53 +1000

    wip: breaking out config info data from viper.AllSettings.

--- lib/config/info.go
+++ lib/config/info.go
@@ -3,6 +3,7 @@ package config
 import (
 	"bytes"
 	"fmt"
+	"strings"
 
 	"github.com/bengarrett/retrotxtgo/lib/logs"
 	"github.com/bengarrett/retrotxtgo/lib/str"
@@ -14,7 +15,20 @@ import (
 func Info(style string) (err logs.IssueErr) {
 	fmt.Println(str.Cp("RetroTxt default configurations when no flags are given."))
 	PrintLocation()
-	out, e := yaml.Marshal(viper.AllSettings())
+	if m := missing(); len(m) > 0 {
+		fmt.Printf("These settings are missing should be configured: %s\n", strings.Join(m, ", "))
+	}
+
+	// ignore unknown or invalid settings in the config file
+	// TODO: break into a func and create a count func
+	// if config is missing settings. list those settings and their values at the bottom of config info
+	var used = make(map[string]interface{})
+	for _, key := range viper.AllKeys() {
+		if d := Defaults[key]; d != nil {
+			used[key] = viper.Get(key)
+		}
+	}
+	out, e := yaml.Marshal(used)
 	if e != nil {
 		return logs.IssueErr{
 			Issue: "failed to read configuration in yaml syntax",
@@ -29,4 +43,17 @@ func Info(style string) (err logs.IssueErr) {
 		str.Highlight(string(out), "yaml", style)
 	}
 	return logs.IssueErr{}
+}
+
+func missing() (list []string) {
+	d, l := len(Defaults), len(viper.AllSettings())
+	if d == l {
+		return list
+	}
+	for key := range Defaults {
+		if !viper.IsSet(key) {
+			list = append(list, key)
+		}
+	}
+	return list
 }
