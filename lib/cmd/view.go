92847b99b9eaa6100870e80ab7cbbfc6efa67829
Ben Garrett <code.by.ben@gmail.com>
2020-06-17 12:25:14 +1000

    Add new & renamed other files to static/text. Much improved handling of view packages.

--- lib/cmd/view.go
+++ lib/cmd/view.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/bengarrett/retrotxtgo/internal/pack"
 	"github.com/bengarrett/retrotxtgo/lib/convert"
@@ -29,41 +30,88 @@ var viewArgs = viewFlags{
 	formats: []string{"color", "text"},
 }
 
+var viewTypes = []string{"chars", "dump", "text"}
+
+type viewPack struct {
+	convert string
+	name    string
+}
+
+// TODO: replace --name with args.
+// have a fileexist check for rare possible conflicting names.
+// retrotxt view filename.txt
+// retrotxt view rt.ansi <- ...
+
+var viewPacks = map[string]viewPack{
+	"437.cr":        {"d", "text/cp437-cr.txt"},
+	"437.crlf":      {"d", "text/cp437-crlf.txt"},
+	"437.lf":        {"d", "text/cp437-lf.txt"},
+	"865":           {"", "text/cp865.txt"},
+	"1252":          {"", "text/cp1252.txt"},
+	"ascii":         {"", "text/retrotxt.asc"},
+	"ansi":          {"", "text/retrotxt.ans"},
+	"ansi.aix":      {"", "text/ansi-aixterm.ans"},
+	"ansi.blank":    {"", "text/ansi-blank"},
+	"ansi.cp":       {"", "text/ansi-cp.ans"},
+	"ansi.cpf":      {"", "text/ansi-cpf.ans"},
+	"ansi.hvp":      {"", "text/ansi-hvp.ans"},
+	"ansi.proof":    {"", "text/ansi-proof.ans"},
+	"ansi.rgb":      {"", "text/ansi-rgb.ans"},
+	"ansi.setmodes": {"", "text/ansi-setmodes.ans"},
+	"iso-1":         {"", "text/iso-8859-1.txt"},
+	"iso-15":        {"", "text/iso-8859-15.txt"},
+	"sauce":         {"", "text/sauce.txt"},
+	"shiftjis":      {"", "text/shiftjis.txt"},
+	"us-ascii":      {"", "text/us-ascii.txt"},
+	"utf8":          {"", "text/utf-8.txt"},
+	"utf8.bom":      {"", "text/utf-8-bom.txt"},
+	"utf16.be":      {"", "text/utf-16-be.txt"},
+	"utf16.le":      {"", "text/utf-16-le.txt"},
+}
+
+func viewPackage() (ok bool, err error) {
+	var s = strings.ToLower(viewArgs.name)
+	if _, err := os.Stat(s); !os.IsNotExist(err) {
+		return false, nil
+	}
+	pkg, exist := viewPacks[s]
+	println(fmt.Sprintf("%+v", pkg), exist)
+	if !exist {
+		return false, nil
+	}
+	b := pack.Get(pkg.name)
+	if b == nil {
+		return false, errors.New("pkg.name is unknown: " + pkg.name)
+	}
+	var r []rune
+	switch pkg.convert {
+	case "d":
+		if r, err = convert.Dump(viewArgs.cp, &b); err != nil {
+			return false, err
+		}
+	case "", "t":
+		if r, err = convert.Text(viewArgs.cp, &b); err != nil {
+			return false, err
+		}
+	}
+	fmt.Println(string(r))
+	return true, nil
+}
+
 // viewCmd represents the view command
 var viewCmd = &cobra.Command{
 	Use:   "view",
 	Short: "Print a legacy text file to the standard output",
 	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			b   []byte
-			r   []rune
-			err error
-		)
-		switch viewArgs.name {
-		case "chars", "characters":
-			b = pack.Get("text/cp-437-all-characters_lf.txt")
-			if b == nil {
-				logs.Log(errors.New("view all characters failed to pack.Get()"))
-			}
-			r, err = convert.Dump(viewArgs.cp, &b)
-			logs.Check("view.convert.text", err)
-			fmt.Println(string(r))
+		fmt.Printf("args: %+v\n", args)
+		ok, err := viewPackage()
+		logs.Check("view.pack", err)
+		if ok {
 			os.Exit(0)
-		case "ansi":
-			b = pack.Get("text/ZII-RTXT.ans")
-			if b == nil {
-				logs.Log(errors.New("view ansi failed to pack.Get()"))
-			}
-		case "ascii":
-			b = pack.Get("text/ascii-logos.txt")
-			if b == nil {
-				logs.Log(errors.New("view ascii failed to pack.Get()"))
-			}
-		default:
-			b, err = filesystem.Read(viewArgs.name)
-			logs.Check("codepage", err)
 		}
-		r, err = convert.Text(viewArgs.cp, &b)
+		b, err := filesystem.Read(viewArgs.name)
+		logs.Check("view.codepage", err)
+		r, err := convert.Text(viewArgs.cp, &b)
 		logs.Check("view.convert.text", err)
 		fmt.Println(string(r))
 	},
