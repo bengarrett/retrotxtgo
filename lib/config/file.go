6f542f49263178a04f4b4845b5842380f64ef5fd
Ben Garrett <code.by.ben@gmail.com>
2020-06-17 18:21:01 +1000

    SetConfig() improved the handling of error states.

--- lib/config/file.go
+++ lib/config/file.go
@@ -31,48 +31,56 @@ func InitDefaults() {
 	}
 }
 
-// SetConfig reads and loads a configuration file.
-func SetConfig(configFlag string) {
-	cfgExit := func(e configErr) {
-		// require manual generation for custom config files
-		e.Err = errors.New("does not exist\n\t use the command: retrotxt config create --config=" + configFlag)
+func exit(e configErr) {
+	if e.Err != nil {
 		fmt.Println(e.String())
 		os.Exit(1)
 	}
+	os.Exit(0)
+}
+
+// SetConfig reads and loads a configuration file.
+func SetConfig(flag string) {
 	viper.SetConfigType("yaml")
-	var configPath = Path()
-	if configFlag != "" {
-		configPath = configFlag
+	var path = flag
+	if flag == "" {
+		path = Path()
 	}
-	viper.SetConfigFile(configPath)
-	if err := viper.ReadInConfig(); err != nil {
-		var e = configErr{
-			FileUsed: viper.ConfigFileUsed(),
-			Err:      err,
+	viper.SetConfigFile(path)
+	var e = configErr{
+		FileUsed: viper.ConfigFileUsed(),
+		Err:      nil,
+	}
+	if e.Err = viper.ReadInConfig(); e.Err != nil {
+		if flag == "" {
+			if errors.Is(e.Err, os.ErrNotExist) {
+				// auto-generate new config except when the --config flag is used
+				e.Err = Create(viper.ConfigFileUsed(), false)
+				exit(e)
+			}
+			// internal config fail
+			exit(e)
 		}
-		if errors.Is(err, os.ErrNotExist) {
+		if errors.Is(e.Err, os.ErrNotExist) {
 			// initialise a new, default config file if conditions are met
-			switch {
-			case len(os.Args) > 2:
+			if len(os.Args) > 2 {
 				switch strings.Join(os.Args[1:3], ".") {
 				case "config.create", "config.delete":
 					// never auto-generate a config when these arguments are given
 					return
 				}
-				if configFlag != "" {
-					cfgExit(e)
-				}
-			case configFlag != "":
-				cfgExit(e)
 			}
-			// auto-generate new config, except when --config flag is used
-			Create(viper.ConfigFileUsed(), false)
+			e.Err = errors.New("does not exist\n\t use the command: retrotxt config create --config=" + flag)
+			exit(e)
 		} else {
-			// config fail
-			fmt.Println(e.String())
-			os.Exit(1)
+			// user config file fail
+			exit(e)
 		}
-	} else if configFlag != "" {
+	} else if flag != "" {
+		if len(viper.AllKeys()) == 0 {
+			e.Err = errors.New("is not a retrotxt config file")
+			exit(e)
+		}
 		// always print the config location when the --config flag is used
 		if len(os.Args) > 0 && os.Args[1] == "config" {
 			// except when the config command is in use
@@ -80,11 +88,20 @@ func SetConfig(configFlag string) {
 		}
 		PrintLocation()
 	}
+	// else internal configuration settings are used
 }
 
 // PrintLocation prints the location of the current configuration file.
 func PrintLocation() {
-	fmt.Println(
-		str.Cb(fmt.Sprintf("Config file: %s",
-			viper.ConfigFileUsed())))
+	s := str.Cb(fmt.Sprintf("Config file: %s",
+		viper.ConfigFileUsed()))
+	d, l := len(Defaults), len(viper.AllKeys())
+	if diff := d - l; diff > 0 {
+		if diff == 1 {
+			s += str.Cb(fmt.Sprint(" (1 setting is missing)"))
+		} else {
+			s += str.Cb(fmt.Sprintf(" (%d settings are missing)", diff))
+		}
+	}
+	fmt.Println(s)
 }
