f1437ae7208c56781349cc2d9f29cf0025628bec
Ben Garrett <code.by.ben@gmail.com>
2020-06-19 08:50:01 +1000

    Renamed Create config keys to HTML.

--- lib/config/config.go
+++ lib/config/config.go
@@ -13,48 +13,49 @@ import (
 
 // Defaults for configuration keys and values.
 var Defaults = map[string]interface{}{
-	"create.layout":            "standard",
-	"create.meta.author":       "",
-	"create.meta.color-scheme": "",
-	"create.meta.description":  "An example",
-	"create.meta.generator":    true,
-	"create.meta.keywords":     "",
-	"create.meta.notranslate":  false,
-	"create.meta.referrer":     "",
-	"create.meta.robots":       "index",
-	"create.meta.theme-color":  "",
-	"create.title":             "RetroTxt | example",
-	"editor":                   "",
-	"save-directory":           home(),
-	"server-port":              8080,
-	"style.info":               "dracula",
-	"style.html":               "lovelace",
+	"editor":                 "",
+	"html.layout":            "standard",
+	"html.meta.author":       "",
+	"html.meta.color-scheme": "",
+	"html.meta.description":  "An example",
+	"html.meta.generator":    true,
+	"html.meta.keywords":     "",
+	"html.meta.notranslate":  false,
+	"html.meta.referrer":     "",
+	"html.meta.robots":       "index",
+	"html.meta.theme-color":  "",
+	"html.title":             "RetroTxt | example",
+	"save-directory":         home(),
+	"server-port":            8080,
+	"style.info":             "dracula",
+	"style.html":             "lovelace",
 }
 
 // Hints provide brief help on the config file configurations.
 var Hints = map[string]string{
-	"create.layout":            "HTML output layout",
-	"create.meta.author":       "defines the name of the page authors",
-	"create.meta.color-scheme": "specifies one or more color schemes with which the page is compatible",
-	"create.meta.description":  "a short and accurate summary of the content of the page",
-	"create.meta.generator":    "include the RetroTxt version and page generation date?",
-	"create.meta.keywords":     "words relevant to the page content",
-	"create.meta.notranslate":  "used to declare that the page should not be translated by Google Translate",
-	"create.meta.referrer":     "controls the Referer HTTP header attached to requests sent from the page",
-	"create.meta.robots":       "behaviour that crawlers from Google, Bing and other engines should use with the page",
-	"create.meta.theme-color":  "indicates a suggested color that user agents should use to customize the display of the page",
-	"create.title":             "page title that is shown in a browser title bar or tab",
-	"editor":                   "text editor to launch when using " + str.Example("config edit"),
-	"save-directory":           "directory to store RetroTxt created HTML files",
-	"server":                   "serve HTML over an internal web server",
-	"server-port":              "port which the internal web server will use",
-	"style.info":               "syntax highlighter for the config info output",
-	"style.html":               "syntax highlighter for html previews",
+	"editor":                 "text editor to launch when using " + str.Example("config edit"),
+	"html.layout":            "HTML output layout",
+	"html.meta.author":       "defines the name of the page authors",
+	"html.meta.color-scheme": "specifies one or more color schemes with which the page is compatible",
+	"html.meta.description":  "a short and accurate summary of the content of the page",
+	"html.meta.generator":    "include the RetroTxt version and page generation date?",
+	"html.meta.keywords":     "words relevant to the page content",
+	"html.meta.notranslate":  "used to declare that the page should not be translated by Google Translate",
+	"html.meta.referrer":     "controls the Referer HTTP header attached to requests sent from the page",
+	"html.meta.robots":       "behaviour that crawlers from Google, Bing and other engines should use with the page",
+	"html.meta.theme-color":  "indicates a suggested color that user agents should use to customize the display of the page",
+	"html.title":             "page title that is shown in a browser title bar or tab",
+	"save-directory":         "directory to store RetroTxt created HTML files",
+	"server":                 "serve HTML over an internal web server",
+	"server-port":            "port which the internal web server will use",
+	"style.info":             "syntax highlighter for the config info output",
+	"style.html":             "syntax highlighter for html previews",
 }
 
 // Settings types and names to be saved in YAML.
 type Settings struct {
-	Create struct {
+	Editor string
+	HTML   struct {
 		Layout string `yaml:"layout"`
 		Meta   struct {
 			Author      string `yaml:"author"`
@@ -69,7 +70,6 @@ type Settings struct {
 		}
 		Title string `yaml:"title"`
 	}
-	Editor        string
 	SaveDirectory string `yaml:"save-directory"`
 	ServerPort    int    `yaml:"server-port"`
 	Style         struct {
@@ -144,28 +144,28 @@ func Marshal() (interface{}, error) {
 	var sc = Settings{}
 	for key, def := range Defaults {
 		switch key {
-		case "create.layout":
-			sc.Create.Layout = def.(string)
-		case "create.meta.author":
-			sc.Create.Meta.Author = def.(string)
-		case "create.meta.color-scheme":
-			sc.Create.Meta.ColorScheme = def.(string)
-		case "create.meta.description":
-			sc.Create.Meta.Description = def.(string)
-		case "create.meta.generator":
-			sc.Create.Meta.Generator = def.(bool)
-		case "create.meta.keywords":
-			sc.Create.Meta.Keywords = def.(string)
-		case "create.meta.notranslate":
-			sc.Create.Meta.Notranslate = def.(bool)
-		case "create.meta.referrer":
-			sc.Create.Meta.Referrer = def.(string)
-		case "create.meta.robots":
-			sc.Create.Meta.Robots = def.(string)
-		case "create.meta.theme-color":
-			sc.Create.Meta.ThemeColor = def.(string)
-		case "create.title":
-			sc.Create.Title = def.(string)
+		case "html.layout":
+			sc.HTML.Layout = def.(string)
+		case "html.meta.author":
+			sc.HTML.Meta.Author = def.(string)
+		case "html.meta.color-scheme":
+			sc.HTML.Meta.ColorScheme = def.(string)
+		case "html.meta.description":
+			sc.HTML.Meta.Description = def.(string)
+		case "html.meta.generator":
+			sc.HTML.Meta.Generator = def.(bool)
+		case "html.meta.keywords":
+			sc.HTML.Meta.Keywords = def.(string)
+		case "html.meta.notranslate":
+			sc.HTML.Meta.Notranslate = def.(bool)
+		case "html.meta.referrer":
+			sc.HTML.Meta.Referrer = def.(string)
+		case "html.meta.robots":
+			sc.HTML.Meta.Robots = def.(string)
+		case "html.meta.theme-color":
+			sc.HTML.Meta.ThemeColor = def.(string)
+		case "html.title":
+			sc.HTML.Title = def.(string)
 		case "editor":
 			sc.Editor = def.(string)
 		case "save-directory":
