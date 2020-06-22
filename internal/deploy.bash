253d208d4381c63455fab7179554274580e63667
Ben Garrett <code.by.ben@gmail.com>
2020-06-22 16:23:27 +1000

    Broke up bash scripts

---
+++ internal/deploy.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# goreleaser requires access to github using a personal access token.
+# https://github.com/settings/tokens
+file=~/.tokens/goreleaser.txt
+if [ -f $FILE ]; then
+    token=$(<$file) && export GITHUB_TOKEN="$token" &&
+        goreleaser --release-notes ../changelog.md --rm-dist &&
+        exit 0
+else
+    echo "The Github token file '$file' could not be found."
+    exit 1
+fi
--- internal/release.bash
+++ internal/release.bash
@@ -20,26 +20,7 @@ git status
 git add ../. &&
     git commit -m "$newcmmt" &&
     git tag -a $newtag -m "$newcmmt" &&
-    git push origin $newtag &&
-    release
-
-# goreleaser requires access to github using a personal access token.
-# https://github.com/settings/tokens
-
-release() {
-    file=~/.tokens/goreleaser.txt
-    if [ -f $FILE ]; then
-        token=$(<$file) && export GITHUB_TOKEN="$token" &&
-            goreleaser --release-notes ../changelog.md --rm-dist &&
-            exit 0
-    else
-        echo "The Github token file '$file' could not be found."
-        exit 1
-    fi
-}
-
-# goreleaser --release-notes changelog.md --rm-dist &&
-#     go get github.com/bengarrett/retrotxtgo
+    git push origin $newtag
 
 # notes
 # to delete a local tag
