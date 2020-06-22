d42786ca207e63da54f00a42ea3f2561e14e6edd
Ben Garrett <code.by.ben@gmail.com>
2020-06-22 13:53:36 +1000

    Test of publishing

--- release.bash
+++ release.bash
@@ -2,8 +2,8 @@
 
 echo "Current version tag $(git describe)"
 
-read -p 'New release semantic version tag? (1.x.x) ' newtag
-#read -p 'New release comment? ' newcmmt
+read -p 'New release semantic version tag? (v1.x.x) ' newtag
+read -p 'New release comment? ' newcmmt
 echo -e "new commit version: \"$newtag\" comment: \"$newcmmt\"\nchangelog:\n$(cat changelog.md)\n"
 read -p 'confirm? [y/N] ' confirm
 
@@ -14,14 +14,19 @@ y | yes | ok) ;;
     ;;
 esac
 
-git status
+# goreleaser can only be used with an active git commit.
 
-git tag -a $newtag -m "$newcmmt" &&
+git status
+git add . &&
+    git commit -m "$newcmmt" &&
+    git tag -a $newtag -m "$newcmmt" &&
     git push origin $newtag
 
 token=$(<~/.tokens/goreleaser.txt) && export GITHUB_TOKEN="$token"
 goreleaser --release-notes changelog.md --rm-dist
-#&&    go get github.com/bengarrett/retrotxtgo
+
+# goreleaser --release-notes changelog.md --rm-dist &&
+#     go get github.com/bengarrett/retrotxtgo
 
 # notes
 # to delete a local tag
