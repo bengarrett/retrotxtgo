fdc61683128e6e42e3c3cc0440dda25ec9bf1337
Ben Garrett <code.by.ben@gmail.com>
2020-06-30 14:30:33 +1000

    convert.List() completed.

--- lib/convert/list.go
+++ lib/convert/list.go
@@ -3,6 +3,7 @@ package convert
 import (
 	"bytes"
 	"fmt"
+	"strconv"
 	"strings"
 	"text/tabwriter"
 
@@ -41,28 +42,35 @@ func Encodings() (e []encoding.Encoding) {
 // List returns a tabled list of supported IANA character set encodings
 func List() *bytes.Buffer {
 	var buf bytes.Buffer
-	header := "\tname\tvalue\tcommon alias"
-	w := new(tabwriter.Writer)
-	w.Init(&buf, 0, 8, 0, '\t', 0)
-	fmt.Fprintln(w, str.Cp(" Known legacy code pages and character encodings"))
-	fmt.Fprintln(w, str.Cf(strings.Repeat("\u2015", 49)))
+	var flags uint = tabwriter.Debug //tabwriter.AlignRight | tabwriter.Debug
+	w := tabwriter.NewWriter(&buf, 0, 0, 2, ' ', flags)
+	header := "formal name\targument value\tnumeric value\talias value\t"
+	fmt.Fprintln(w, str.Cp(" Supported legacy code pages and character encodings"))
 	fmt.Fprintln(w, header)
 	for _, e := range Encodings() {
-		n, v, a := cells(e)
-		fmt.Fprintf(w, "\t%s\t%s\t%s\n", n, str.Ci(v), a) // name, value, alias
+		n, v, d, a := cells(e)
+		// do not use ANSI colors in cells as it will break the table layout!
+		fmt.Fprintln(w, fmt.Sprintf(" %s\t %s\t %s\t %s\t", n, v, d, a)) // name, value, numeric, alias
 	}
-	fmt.Fprintln(w, header)
-	fmt.Fprint(w, "\n"+str.Cinf("*")+" IBM Code Page 437 ("+str.Cc("CP437")+") is commonly used by MS-DOS English text and ANSI art")
-	fmt.Fprint(w, "\n  ISO 8859-1 ("+str.Cc("Latin1")+") is found in legacy Internet, Unix and Amiga documents")
-	fmt.Fprint(w, "\n  Windows 1252 ("+str.Cc("CP1252")+") is found in legacy English language Windows operating systems")
+	fmt.Fprintln(w, "\nEither argument, numeric or alias values are valid codepage arguments")
+	fmt.Fprintln(w, "All these codepage flags will match ISO 8859-1")
+	fmt.Fprintln(w, str.Example("retrotxt view ascii -c=iso-8859-1"))
+	fmt.Fprintln(w, str.Example("retrotxt view ascii -c1"))
+	fmt.Fprintln(w, str.Example("retrotxt view ascii -c=latin1"))
+	fmt.Fprintln(w, "\n"+str.Cinf("*")+" IBM Code Page 437 ("+str.Cc("cp437")+") is commonly used on MS-DOS and with ANSI art")
+	fmt.Fprintln(w, "  ISO 8859-1 ("+str.Cc("latin1")+") is found on legacy Unix, Amiga and the early Internet")
+	fmt.Fprintln(w, "  Windows 1252 ("+str.Cc("cp1252")+") is found on legacy Windows 9x and earlier systems")
+	fmt.Fprintln(w, "  Macintosh ("+str.Cc("macintosh")+") is found on Mac OS 9 and earlier systems")
+	fmt.Fprintln(w, "  Modern systems and the web today use UTF-8, a Unicode encoding that's a subset of ISO 8859-1 which itself is a subset of US-ASCII")
 	w.Flush()
 	return &buf
 }
 
-func cells(e encoding.Encoding) (n, v, a string) {
+func cells(e encoding.Encoding) (n, v, d, a string) {
 	if e == nil {
-		return n, v, a
+		return n, d, v, a
 	}
+	n = fmt.Sprint(e)
 	var err error
 	if v, err = htmlindex.Name(e); err == nil {
 		a, _ = ianaindex.MIME.Name(e)
@@ -70,8 +78,15 @@ func cells(e encoding.Encoding) (n, v, a string) {
 		v, _ = ianaindex.MIME.Name(e)
 	}
 	v = strings.ToLower(uniform(v))
+	s1 := strings.Split(n, " ")
+	s2 := strings.Split(n, "-")
+	if i, err := strconv.Atoi(s1[len(s1)-1]); err == nil {
+		d = fmt.Sprint(i)
+	} else if i, err := strconv.Atoi(s2[len(s2)-1]); err == nil {
+		d = fmt.Sprint(i)
+	}
 	a = alias(a, v, e)
-	return fmt.Sprint(e), v, a
+	return n, v, d, a
 }
 
 func alias(s, val string, e encoding.Encoding) string {
@@ -87,6 +102,8 @@ func alias(s, val string, e encoding.Encoding) string {
 			return "latinI"
 		case "cp852":
 			return "latinII"
+		case "macintosh":
+			return "mac"
 		}
 		a, _ = ianaindex.MIB.Name(e)
 		a = strings.ToLower(a)
