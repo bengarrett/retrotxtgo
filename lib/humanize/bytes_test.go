ded0fdfe9f54616ac46acb1a1e2e9271f0784d23
Ben Garrett <code.by.ben@gmail.com>
2020-11-22 14:26:22 +1100

    new decimal test.

--- lib/humanize/bytes_test.go
+++ lib/humanize/bytes_test.go
@@ -6,8 +6,9 @@ import (
 	"golang.org/x/text/language"
 )
 
-func TestBytes(t *testing.T) {
-	var us = language.AmericanEnglish
+var us = language.AmericanEnglish
+
+func Test_binary(t *testing.T) {
 	type args struct {
 		b int64
 		t language.Tag
@@ -26,6 +27,30 @@ func TestBytes(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Binary(tt.args.b, tt.args.t); got != tt.want {
 				t.Errorf("Binary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_decimal(t *testing.T) {
+	type args struct {
+		b int64
+		t language.Tag
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"0", args{int64(0), us}, "0"},
+		{"1", args{int64(1), us}, "1B"},
+		{"1.5K", args{int64(1500), us}, "1.5 kB"},
+		{"2.5M", args{int64(2500000), us}, "2.50 MB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := decimal(tt.args.b, tt.args.t); got != tt.want {
+				t.Errorf("decimal() = %v, want %v", got, tt.want)
 			}
 		})
 	}
