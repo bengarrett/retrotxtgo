826d4c199292556be5c3fd188b8471544c55991f
Ben Garrett <code.by.ben@gmail.com>
2020-11-24 09:18:45 +1100

    Updated types and funcs.

--- lib/filesystem/reader_test.go
+++ lib/filesystem/reader_test.go
@@ -9,13 +9,13 @@ func TestNewlines(t *testing.T) {
 	tests := []struct {
 		name string
 		text []rune
-		want NL
+		want LB
 	}{
 		{"unix", []rune("hello\x0aworld\x0a"), LF()},
 		{"win", []rune("hello\x0d\x0aworld\x0d\x0a\x1a"), CRLF()},
 		{"c64", []rune("hello\x0dworld\x0d"), CR()},
-		{"ibm", []rune("hello\x15world\x15"), EBCDIC()},
-		{"mix", []rune("\x15Windows newline: \x0d\x0a\x15Unix newline: \x0a\x15"), EBCDIC()},
+		{"ibm", []rune("hello\x15world\x15"), NL()},
+		{"mix", []rune("\x15Windows newline: \x0d\x0a\x15Unix newline: \x0a\x15"), NL()},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -28,7 +28,7 @@ func TestNewlines(t *testing.T) {
 
 func TestNewline(t *testing.T) {
 	type args struct {
-		r         NL
+		r         LB
 		extraInfo bool
 	}
 	tests := []struct {
@@ -37,8 +37,8 @@ func TestNewline(t *testing.T) {
 		want string
 	}{
 		{"empty", args{}, "??"},
-		{"nl", args{EBCDIC(), false}, "NL"},
-		{"nl", args{EBCDIC(), true}, "NL (IBM EBCDIC)"},
+		{"nl", args{NL(), false}, "NL"},
+		{"nl", args{NL(), true}, "NL (IBM EBCDIC)"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
