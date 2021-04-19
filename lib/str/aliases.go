951a3233e5db8cb364e56edcde7c8c1e45ba8819
Ben Garrett <code.by.ben@gmail.com>
2021-04-19 14:26:39 +1000

    Expanded func comments.

--- lib/str/aliases.go
+++ lib/str/aliases.go
@@ -31,55 +31,53 @@ func Info() string {
 
 // color aliases.
 
-// Cb secondary.
+// Cb returns a string in the color named secondary.
 func Cb(t string) string {
 	return color.Secondary.Sprint(t)
 }
 
-// Cc comment.
+// Cc returns a string in the color named comment.
 func Cc(t string) string {
 	return color.Comment.Sprint(t)
 }
 
-// Ce warn.
+// Ce returns a string in the color named warn.
 func Ce(t string) string {
 	return color.Warn.Sprint(t)
 }
 
-// Cf fuzzy.
+// Cf returns a string in the style named fuzzy.
 func Cf(t string) string {
 	return color.OpFuzzy.Sprint(t)
 }
 
-// Ci italic.
+// Ci returns a string in the style named italic.
 func Ci(t string) string {
 	return color.OpItalic.Sprint(t)
 }
 
-// Cinf info.
+// Cinf returns a string in the color named info.
 func Cinf(t string) string {
 	return color.Info.Sprint(t)
 }
 
-// Cp primary.
+// Cp returns a string in the color named primary.
 func Cp(t string) string {
 	return color.Primary.Sprint(t)
 }
 
-// Cs success.
+// Cs returns a string in the color named success.
 func Cs(t string) string {
 	return color.Success.Sprint(t)
 }
 
-// Bool returns a checkmark ✓ or cross ✗.
+// Bool returns a checkmark ✓ when true or a cross ✗ when false.
 func Bool(b bool) string {
 	const check, cross = "✓", "✗"
-	switch b {
-	case true:
+	if b {
 		return color.Success.Sprint(check)
-	default:
-		return color.Warn.Sprint(cross)
 	}
+	return color.Warn.Sprint(cross)
 }
 
 // Default appends (default ...) to the usage string.
