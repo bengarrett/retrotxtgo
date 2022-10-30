307b30b354af3274ab7b2beac592db6e335f6de9
Ben Garrett <code.by.ben@gmail.com>
2022-10-30 20:51:51 +0800

    removed unused config funcs.

--- lib/config/config.go
+++ lib/config/config.go
@@ -2,19 +2,7 @@
 package config
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/bengarrett/retrotxtgo/lib/config/internal/get"
-	"github.com/bengarrett/retrotxtgo/lib/config/internal/set"
-	"github.com/bengarrett/retrotxtgo/meta"
-	"github.com/spf13/viper"
-)
-
-var (
-	ErrEditorNil = errors.New("no suitable text editor can be found")
-	ErrEditorRun = errors.New("editor cannot be run")
-	ErrLogo      = errors.New("program logo is missing")
 )
 
 // Formats the choices for command flags.
@@ -27,67 +15,6 @@ func Format() Formats {
 	return Formats{
 		Info: [5]string{"color", "json", "json.min", "text", "xml"},
 	}
-}
-
-func CmdPath() string {
-	return fmt.Sprintf("%s config", meta.Bin)
-}
-
-// Enabled returns all the Viper keys holding a value that are used.
-// This will hide all unrecognized manual edits to the configuration file.
-func Enabled() map[string]interface{} {
-	sets := make(map[string]interface{})
-	for _, key := range viper.AllKeys() {
-		if d := get.Reset()[key]; d != nil {
-			sets[key] = viper.Get(key)
-		}
-	}
-	return sets
-}
-
-// SortKeys list all the available configuration setting names sorted by hand.
-func SortKeys() []string {
-	all := set.Keys()
-	keys := []string{
-		get.FontFamily,
-		get.Title,
-		get.LayoutTmpl,
-		get.FontEmbed,
-		get.SaveDir,
-		get.Serve,
-		get.Editor,
-		get.Styleh,
-		get.Stylei,
-	}
-	for _, key := range all {
-		found := false
-		for _, used := range keys {
-			if key == used {
-				found = true
-				break
-			}
-		}
-		if !found {
-			keys = append(keys, key)
-		}
-	}
-	return keys
-}
-
-// Missing returns the settings that are not found in the configuration file.
-// This could be due to new features being added after the file was generated
-// or because of manual file edits.
-func Missing() []string {
-	list := []string{}
-	if len(get.Reset()) == len(viper.AllSettings()) {
-		return list
-	}
-	for key := range get.Reset() {
-		if !viper.IsSet(key) {
-			list = append(list, key)
-		}
-	}
-	return list
 }
 
 // Tip provides some brief help information of the configurations.
--- lib/config/config_test.go
+++ lib/config/config_test.go
@@ -5,41 +5,8 @@ import (
 
 	"github.com/bengarrett/retrotxtgo/lib/config"
 	"github.com/bengarrett/retrotxtgo/lib/config/internal/get"
-	"github.com/bengarrett/retrotxtgo/lib/config/internal/set"
 	"github.com/gookit/color"
 )
-
-func TestMissing(t *testing.T) {
-	tests := []struct {
-		name      string
-		wantCount int
-	}{
-		{"want no values", len(set.Keys())},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotList := len(config.Missing()); gotList != tt.wantCount {
-				t.Errorf("Missing() = %v, want %v", gotList, tt.wantCount)
-			}
-		})
-	}
-}
-
-func TestSortKeys(t *testing.T) {
-	got, keys := config.SortKeys(), set.Keys()
-	lenG, lenK := len(got), len(keys)
-	if lenG == 0 {
-		t.Errorf("SortKeys() is empty")
-		return
-	}
-	if lenG != lenK {
-		t.Errorf("SortKeys() length = %d, want %d", lenG, lenK)
-	}
-	const want = get.FontFamily
-	if s := got[0]; s != want {
-		t.Errorf("SortKeys()[0] = %s, want %s", s, want)
-	}
-}
 
 func TestTip(t *testing.T) {
 	color.Enable = false
